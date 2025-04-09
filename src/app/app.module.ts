8dad94ef1975ee8862d625acdfffc58833e4979e
Aliasger Jaffer <aliasger.jaffer@gmail.com>
2025-04-09 15:50:39 -0400

    Working aux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MyLibComponent, MyLibModule} from "my-lib";
+import {RouterModule, RouterOutlet} from "@angular/router";
+import {HelloComponent} from "./hello.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyLibModule,
+    RouterModule.forRoot([
+      {
+        path: 'keys',
+        component: MyLibComponent,
+        children: [
+          {
+            path: 'home',
+            component: HelloComponent,
+            outlet: 'outlet' // this outlet exists in MyLibComponent's template
+          }
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
